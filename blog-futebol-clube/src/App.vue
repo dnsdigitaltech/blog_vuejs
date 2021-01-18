ee7a0047100d3997dc8e44ce3641a9388e5ae079
Davi Bernardo <dnssites@gmail.com>
2021-01-18 13:51:45 -0300

    Praticando: criando o componente Header

--- blog-futebol-clube/src/App.vue
+++ blog-futebol-clube/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <component-header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ComponentHeader from './components/ComponentHeader'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ComponentHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css?family=Rajdhani&display=swap');
 </style>
