093c56af7ba1f690d16bd59d6fd3322dd519d7ac
dnsdigitaltech <dnssites@gmail.com>
2021-01-20 11:30:18 -0300

    Praticando: informando props para o componente news

--- blog-futebol-clube/src/components/ComponentSectionNews.vue
+++ blog-futebol-clube/src/components/ComponentSectionNews.vue
@@ -1,9 +1,29 @@
 <template>
     <section>
          <div class="container">
-            <ComponentSectionNewsIndividual/>   
-            <ComponentSectionNewsIndividual/> 
-            <ComponentSectionNewsIndividual/>           
+            <ComponentSectionNewsIndividual
+                img-name="news1.jpg"
+                img-info="Notícia 1"
+                news-title="Começam os treinos para nova temporada"
+                news-content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fugiat minima minus laboriosam sequi iste eius quae maiores obcaecati exercitationem aperiam voluptatibus dicta accusamus, necessitatibus, ipsa quaerat eveniet? Sequi, cumque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, eos dolores! Inventore ipsum saepe commodi nihil voluptas repellat veniam quod animi praesentium tenetur iste accusantium, vel nesciunt accusamus porro minima."
+                news-date="20/02/2021"
+            /> 
+
+            <ComponentSectionNewsIndividual
+                img-name="news2.jpg"
+                img-info="Notícia 1"
+                news-title="Começam os treinos para nova temporada"
+                news-content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fugiat minima minus laboriosam sequi iste eius quae maiores obcaecati exercitationem aperiam voluptatibus dicta accusamus, necessitatibus, ipsa quaerat eveniet? Sequi, cumque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, eos dolores! Inventore ipsum saepe commodi nihil voluptas repellat veniam quod animi praesentium tenetur iste accusantium, vel nesciunt accusamus porro minima."
+                news-date="20/02/2021"
+            /> 
+
+            <ComponentSectionNewsIndividual
+                img-name="news3.jpg"
+                img-info="Notícia 1"
+                news-title="Começam os treinos para nova temporada"
+                news-content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fugiat minima minus laboriosam sequi iste eius quae maiores obcaecati exercitationem aperiam voluptatibus dicta accusamus, necessitatibus, ipsa quaerat eveniet? Sequi, cumque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, eos dolores! Inventore ipsum saepe commodi nihil voluptas repellat veniam quod animi praesentium tenetur iste accusantium, vel nesciunt accusamus porro minima."
+                news-date="20/02/2021"
+            />            
          </div>
      </section>
 </template>
--- blog-futebol-clube/src/components/ComponentSectionNewsIndividual.vue
+++ blog-futebol-clube/src/components/ComponentSectionNewsIndividual.vue
@@ -1,23 +1,40 @@
 <template>
      <div class="row">
          <div class="col-3">
-             <img src="../assets/news1.jpg" alt="Notícia 1">
+             <img :src="require('../assets/' + this.imgName)" :alt="imgInfo">
          </div>
          <div class="col-9">
-             <h2>Começam os treinos para nova temporada</h2>
-             <p>
-                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fugiat minima minus laboriosam sequi iste eius quae maiores obcaecati exercitationem aperiam voluptatibus dicta accusamus, necessitatibus, ipsa quaerat eveniet? Sequi, cumque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, eos dolores! Inventore ipsum saepe commodi nihil voluptas repellat veniam quod animi praesentium tenetur iste accusantium, vel nesciunt accusamus porro minima. 
-             </p>
-             <span class="font-italic">
-                 20/02/2021
-             </span>
+             <h2>{{ newsTitle }}</h2>
+             <p>{{ newsContent | truncate(200) }}</p>
+             <span class="font-italic">{{newsDate}}</span>
          </div>
      </div>
 </template>
 
 <script>
     export default {
-
+        props: {
+            imgName: {
+                type: String,
+                required: true
+            },
+            imgInfo: {
+                type: String,
+                required: true
+            },
+            newsTitle: {
+                type: String,
+                required: true
+            },
+            newsContent: {
+                type: String,
+                required: true
+            },
+            newsDate: {
+                type: String,
+                required: true
+            }
+        }
     }
 </script>
 
