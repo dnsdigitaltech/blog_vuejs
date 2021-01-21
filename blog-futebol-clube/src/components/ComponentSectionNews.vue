ec4efe35a86fb9dc5feaaa3c3cb9bfb459bf91be
Davi Bernardo <dnssites@gmail.com>
2021-01-21 16:14:17 -0300

    Praticando: Ciclo de Vida de um Componente

--- blog-futebol-clube/src/components/ComponentSectionNews.vue
+++ blog-futebol-clube/src/components/ComponentSectionNews.vue
@@ -2,28 +2,14 @@
     <section>
          <div class="container">
             <ComponentSectionNewsIndividual
-                img-name="news1.jpg"
-                img-info="Notícia 1"
-                news-title="Começam os treinos para nova temporada"
-                news-content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fugiat minima minus laboriosam sequi iste eius quae maiores obcaecati exercitationem aperiam voluptatibus dicta accusamus, necessitatibus, ipsa quaerat eveniet? Sequi, cumque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, eos dolores! Inventore ipsum saepe commodi nihil voluptas repellat veniam quod animi praesentium tenetur iste accusantium, vel nesciunt accusamus porro minima."
-                news-date="20/02/2021"
-            /> 
-
-            <ComponentSectionNewsIndividual
-                img-name="news2.jpg"
-                img-info="Notícia 1"
-                news-title="Começam os treinos para nova temporada"
-                news-content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fugiat minima minus laboriosam sequi iste eius quae maiores obcaecati exercitationem aperiam voluptatibus dicta accusamus, necessitatibus, ipsa quaerat eveniet? Sequi, cumque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, eos dolores! Inventore ipsum saepe commodi nihil voluptas repellat veniam quod animi praesentium tenetur iste accusantium, vel nesciunt accusamus porro minima."
-                news-date="20/02/2021"
-            /> 
-
-            <ComponentSectionNewsIndividual
-                img-name="news3.jpg"
-                img-info="Notícia 1"
-                news-title="Começam os treinos para nova temporada"
-                news-content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fugiat minima minus laboriosam sequi iste eius quae maiores obcaecati exercitationem aperiam voluptatibus dicta accusamus, necessitatibus, ipsa quaerat eveniet? Sequi, cumque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, eos dolores! Inventore ipsum saepe commodi nihil voluptas repellat veniam quod animi praesentium tenetur iste accusantium, vel nesciunt accusamus porro minima."
-                news-date="20/02/2021"
-            />            
+                v-for="notice in news"
+                :key="notice.id"
+                :img-name="notice.img"
+                :img-info="notice.imgInfo"
+                :news-title="notice.title"
+                :news-content="notice.content"
+                :news-date="notice.date"
+            />        
          </div>
      </section>
 </template>
@@ -33,7 +19,38 @@
     export default {
         components: {
             ComponentSectionNewsIndividual
-        }
+        },
+        data() {
+            return {
+                news: []
+            }
+        },
+        created() {
+            this.news = [{
+                id:1,
+                title: 'Começam os treinos para nova temporada',
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fugiat minima minus laboriosam sequi iste eius quae maiores obcaecati exercitationem aperiam voluptatibus dicta accusamus, necessitatibus, ipsa quaerat eveniet? Sequi, cumque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, eos dolores! Inventore ipsum saepe commodi nihil voluptas repellat veniam quod animi praesentium tenetur iste accusantium, vel nesciunt accusamus porro minima.',
+                date: '2021-01-21',
+                img: 'news1.jpg',
+                imgInfo: 'Notícia 1'
+            },
+            {
+                id:2,
+                title: 'Jogo de quarta-feira termina empatado',
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fugiat minima minus laboriosam sequi iste eius quae maiores obcaecati exercitationem aperiam voluptatibus dicta accusamus, necessitatibus, ipsa quaerat eveniet? Sequi, cumque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, eos dolores! Inventore ipsum saepe commodi nihil voluptas repellat veniam quod animi praesentium tenetur iste accusantium, vel nesciunt accusamus porro minima.',
+                date: '2021-01-11',
+                img: 'news2.jpg',
+                imgInfo: 'Notícia 2'
+            },
+            {
+                id:3,
+                title: 'A inauguração do novo estádio será semana  que vem',
+                content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus fugiat minima minus laboriosam sequi iste eius quae maiores obcaecati exercitationem aperiam voluptatibus dicta accusamus, necessitatibus, ipsa quaerat eveniet? Sequi, cumque! Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, eos dolores! Inventore ipsum saepe commodi nihil voluptas repellat veniam quod animi praesentium tenetur iste accusantium, vel nesciunt accusamus porro minima.',
+                date: '2021-01-28',
+                img: 'news3.jpg',
+                imgInfo: 'Notícia 3'
+            }]
+        },
     }
 </script>
 
