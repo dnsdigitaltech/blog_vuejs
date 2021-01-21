8a61d3cb591f902f7eefe5392afe01a1178cc591
Davi Bernardo <dnssites@gmail.com>
2021-01-21 14:07:36 -0300

    baixar conteudo

---
+++ blog-futebol-clube/src/components/ComponentSectionNews.vue
@@ -0,0 +1,45 @@
+<template>
+    <section class="container">
+        <ComponentSectionNewsIndividual/>
+    </section>
+</template>
+
+
+<script>
+    import ComponentSectionNewsIndividual from './ComponentSectionNewsIndividual'
+    export default {
+        components: {
+            ComponentSectionNewsIndividual
+        }
+    }
+</script>
+
+
+<style scoped>
+    section {
+        margin-top: 25px;
+    }
+    .container .row {
+        height: 300px;
+    }
+    .container .row .col-5:first-child {
+        background-color: rgba(0, 0, 0. 0.5);
+        background-image: url('../assets/football.jpg');
+        background-repeat: no-repeat;
+        background-size: 100%;
+    }
+    .container .row .col-5:nth-child(2) {
+        background-image: url('../assets/football2.jpg');
+        background-repeat: no-repeat;
+        background-size: 100%;
+    }
+    h2 {
+        margin: 25% 0;
+        color: #f6b64e;
+        background-color: rgba(0, 0, 0, 0.4);
+        padding: 10px;
+    }
+    .principal-news {
+        cursor: pointer;
+    }
+</style>
---
+++ blog-futebol-clube/src/components/ComponentSectionNewsIndividual.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row">
+        <div class="col-3">
+            <img src="../assets/news1.jpg" alt="Notícia 1">
+        </div>
+        <div class="col9">
+            <h2>Comecem os treinos para a nova temporada</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum voluptatem impedit in excepturi doloribus quaerat quod praesentium non temporibus! Modi minima sequi maxime quidem, blanditiis adipisci quam aspernatur accusantium consectetur.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    
+</style>
